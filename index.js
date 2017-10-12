52d43106410e37e52d18ac5eb342ed9e971cb706
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-10-12 14:39:46 +0200

    :bug: Better xls parser, prevent several parser errors

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 #!/usr/bin/env node
 
-const xlsParse = require('xls-parse');
-const toCSV = require('array-to-csv');
-const inputFile = process.argv[2] || './input.xls';
-const outputFile = process.argv[3] || './output.csv';
-const sheetName = 'report1';
-let data = xlsParse.formatXls2Obj(inputFile, sheetName);
+var XLSX = require('xlsx')
+const toCSV = require('array-to-csv')
+const inputFile = process.argv[2] || './input.xls'
+const outputFile = process.argv[3] || './output.csv'
+const SHEET_NAME = 'report1'
 
-data = data[sheetName];
-// remove header
-data.splice(0, 5);
+const COL_FECHA = 'A'
+const COL_DESCRIPCIÓN = 'C'
+const COL_CONCEPTO = 'D'
+const COL_IMPORTE = 'E'
+const COL_SALDO = 'F'
+const ROW_OFFSET = 6
+
+var workbook = XLSX.readFile(inputFile)
+var LENGTH = getWorkbookLength(workbook) - ROW_OFFSET
+
+function getWorkbookLength(workbook) {
+  const lastKey = Object.keys(workbook.Sheets[SHEET_NAME]).length - 4
+  return +Object.keys(workbook.Sheets[SHEET_NAME])[lastKey].slice(1, Infinity)
+}
+
+function getValue(colName, row) {
+  const value = workbook.Sheets[SHEET_NAME][colName + (row + ROW_OFFSET)] ?
+  workbook.Sheets[SHEET_NAME][colName + (row + ROW_OFFSET)].v : ''
+  if (colName === COL_IMPORTE) {
+    return value.replace('.', '').replace(',', '.')
+  }
+  return value
+}
 
 // ING Spain export format
 // ['FECHA', '', 'DESCRIPCIÓN', 'CONCEPTO', 'IMPORTE', 'SALDO', '']
-// Original statement 
-// amount is splited in 2 cells due number format
-// ['09/06/2017', '', 'Transferencia to XXX', 'INV-0001', '"-2.640', '83"', '"3.988', '25"', '']
-
-data = data.map((statement) => {
-  var amount = statement[4].replace(/"|\./g, '') + '.' + statement[5].replace(/"|\./g, '');
-  return [statement[0], +amount, null, statement[2], statement[3], null];
-});
-
 // Xero CSV format header
 // *Date,*Amount,Payee,Description,Reference,Check Number
-// console.log(toCSV(data));
-var fs = require('fs');
+
+const data = []
+for (let i = 0; i <= LENGTH; i++) {
+  var amount = getValue(COL_IMPORTE, i)
+  if (isNaN(+amount)) {
+    console.log(amount, +amount, i)
+  }
+  data.push([
+    getValue(COL_FECHA, i), +amount,
+    null,
+    getValue(COL_DESCRIPCIÓN, i),
+    getValue(COL_CONCEPTO, i),
+    null
+  ])
+}
+
+var fs = require('fs')
 fs.writeFile(outputFile, toCSV(data), function (err) {
   if (err) {
-    return console.log(err);
+    return console.log(err)
   }
-  console.log(`DONE => ${outputFile}`);
-});
+  console.log(`DONE => ${outputFile}`)
+})
